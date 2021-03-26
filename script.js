1794a46a9bf97f6aa510f395d170cdb86d62970a
Nick Tillinghast <nickt2@cox.net>
2021-03-26 07:23:42 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+const weightMark = 78;
+const weightJohn = 92;
+
+let bmiMark = weightMark / heightMark ** 2;
+let bmiJohn = heightJohn / weightJohn ** 2;
+
+console.log(bmiMark);
+console.log(bmiJohn);
